bcb5703a6da9aa549ef2d68613651d259b0a4101
anailson ribeiro <arsantos315@gmail.com>
2021-04-20 14:11:22 -0300

    Conhecendo objeto Date

--- src/modulo_datas/DatasEmJava.java
+++ src/modulo_datas/DatasEmJava.java
@@ -1,11 +1,13 @@
 package modulo_datas;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatasEmJava {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		
 		Date date = new Date();
@@ -26,6 +28,26 @@ public class DatasEmJava {
 		System.out.println("Ano: " + date.getYear()); //1900 + 121 - data atual  + 1900 (2021)
 		
 		System.out.println("Ano: " + (date.getYear() + 1900));
+		
+		System.out.println("------------------------------------------------------------");
+		
+		/*---------------SIMPLES DATE FORMAT----------------*/
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy  HH:mm.ss");
+		
+		System.out.println("Data atual em formato padrão e String: " + simpleDateFormat.format(date));
+		
+		
+		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");
+		
+		System.out.println("Data em formato para banco de dados: " + simpleDateFormat.format(date));
+		
+		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println("Objeto Date: " + simpleDateFormat.parse("1984-05-30 "));
+		
+		
+		simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		System.out.println("Objeto Date: " + simpleDateFormat.parse("30-05-1984 "));
 		
 	}
 }
